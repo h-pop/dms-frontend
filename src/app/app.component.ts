ac8cacec9de89bce6c845cc0886426bf4fa7d358
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2019-07-02 15:21:46 +0200

    #: Messing with http requests.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dms-frontend';
+  greeting = {'id': 'XXX', 'content': 'Hello World'};
+  greeting2 = {}
+  constructor(private http: HttpClient) { 
+	  http.get('/api/v1/protectedRequest').subscribe(data => this.greeting2 = data);
+  }
 }
