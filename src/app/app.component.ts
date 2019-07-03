485648aad3ada18dcaf34036fef049cc397b7aab
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2019-07-03 12:28:58 +0200

    #: Set API_URL env variable for communication with backend api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dms-frontend';
-  greeting = {'id': 'XXX', 'content': 'Hello World'};
-  greeting2 = {}
-  constructor(private http: HttpClient) { 
-	  http.get('/api/v1/protectedRequest').subscribe(data => this.greeting2 = data);
-  }
+    title = 'Custom Document Management System';
+    greeting = {};
+    constructor(private http: HttpClient) {
+        http.get(environment.API_URL + '/login/testRequest')
+            .subscribe(data => this.greeting = data);
+    }
 }
