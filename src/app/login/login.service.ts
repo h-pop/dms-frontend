7fa956e74605922549e3fd08dd0d29f3fbfe0bdd
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2019-07-04 22:31:13 +0200

    #: OAuth2

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
@@ -39,6 +39,21 @@ export class LoginService {
     }
 
     public obtainAccessToken(credentials) {
-        
+        const body = new HttpParams()
+            .set('username', credentials.username)
+            .set('password', credentials.password)
+            .set('grant_type', 'password');
+
+        const headers = {
+            'Authorization': 'Basic ' + btoa('dms-user:dms-secret'),
+            'Content-type': 'application/x-www-form-urlencoded'
+        };
+        this.http.post('http://localhost:8080/' + 'oauth/token', body.toString(), { headers }).subscribe(data => {
+            window.sessionStorage.setItem('token', JSON.stringify(data));
+            console.log(window.sessionStorage.getItem('token'));
+            // this.router.navigate(['list-user']);
+        }, error => {
+            alert(error.error.error_description);
+        });
     }
 }
