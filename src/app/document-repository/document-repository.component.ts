c6c676556f10fb7feda457658589c3c8c00bfdbc
hpopiolkiewicz <h.popiolkiewicz@gmail.com>
2024-06-09 09:30:29 +0200

    Using own s-table js library to display table with data

--- src/app/document-repository/document-repository.component.ts
+++ src/app/document-repository/document-repository.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+declare var sTable;
 
 @Component({
   selector: 'app-document-repository',
   templateUrl: './document-repository.component.html',
   styleUrls: ['./document-repository.component.css']
 })
-export class DocumentRepositoryComponent implements OnInit {
+export class DocumentRepositoryComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    const container = document.querySelector('#s-table-container');
+    sTable.create(container);
+  }
 }
